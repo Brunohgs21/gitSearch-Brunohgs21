951ab02c6305562586b22ca9d98b4b14b2fe610b
Bruno <brunohgs21@outlook.com>
2022-10-15 22:06:27 -0300

    quase finalizado

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,180 @@
 /* Desenvolva sua lógica aqui...*/
+
+
+
+
+function formUser() {
+    const btn = document.querySelector('#user-button')
+
+    btn.addEventListener('click', (event) => {
+        event.preventDefault()
+        console.log(event.target)
+
+        let p = event.target.previousElementSibling
+       console.log(p)
+        let user = p.previousElementSibling.value
+        console.log(user)
+        searching(user)
+
+
+    })
+
+
+}
+formUser()
+
+
+async function searching(usuario) {
+    //console.log(usuario)
+
+    const data = await fetch(`https://api.github.com/users/${usuario}`)
+    const dataJson = await data.json()
+
+
+
+
+
+    if (dataJson.message) {
+        const result = document.querySelector('.result')
+        result.classList = 'show-result'
+        console.log(dataJson.message)
+    } else {
+        const btn = document.querySelector('#user-button')
+        btn.classList.toggle('button-loading')
+        searchUser(dataJson)
+
+         setTimeout(() => {
+
+              window.location.href = '../profile/index.html'
+         }, 4000)
+    }
+
+
+    return dataJson
+
+
+
+}
+
+
+function getDataLocalStorage() {
+    return JSON.parse(localStorage.getItem("@gitsearch:user")) || []
+}
+
+
+
+
+
+function searchUser(user) {
+    console.log(user)
+
+
+
+    let users = getDataLocalStorage()
+    console.log(users)
+
+    let { login, avatar_url, bio, name, repos_url, id } = user
+    console.log(login)
+
+    let newUser = {
+        'login': login,
+        'avatar_url': avatar_url,
+        'bio': bio,
+        'name': name,
+        'repos_url': repos_url,
+        'id': id,
+    }
+
+    console.log(newUser)
+
+
+    if (users.length < 3) {
+
+
+        users = [...users, newUser]
+
+
+
+
+    } else {
+        users.splice(0, 1)
+
+        users = [...users, newUser]
+
+
+    }
+
+    localStorage.setItem("@gitsearch:user", JSON.stringify(users))
+    console.log(getDataLocalStorage())
+}
+
+{/* <li class="found-user">
+                <img class='found-img'src="" alt="">
+                <button class="found-btn">Acessar este perfil</button>
+              </li> */}
+
+function listRecents() {
+    let recents = getDataLocalStorage()
+
+    let ul = document.querySelector('.found-users')
+
+    recents.forEach((user) => {
+        let recentUser = createRecent(user)
+
+        ul.appendChild(recentUser)
+    })
+}
+listRecents()
+
+function createRecent(element) {
+    let { avatar_url, id } = element
+
+
+    let li = document.createElement('li')
+    li.classList.add('found-user')
+
+    let img = document.createElement('img')
+    img.classList.add('found-img')
+    img.src = avatar_url
+
+
+
+    let btn = document.createElement('button')
+    btn.classList.add('found-btn')
+    btn.innerText = 'Acessar este perfil'
+    btn.id = id
+
+    img.addEventListener('mouseover', () => {
+        btn.classList = 'found-btn-2'
+    })
+    li.addEventListener('mouseleave',()=>{
+        btn.classList = 'found-btn'
+    })
+
+    btn.addEventListener('click', (event) => {
+        let idUser = event.target.id
+        console.log
+        let data = getDataLocalStorage()
+        let filter = data.find((element) => {
+            return element.id == idUser
+        })
+
+    })
+
+    li.append(img, btn)
+
+    return li
+
+}
+
+
+ function activeBtn() {
+     const input = document.querySelector('.user-input')
+    
+
+     input.addEventListener('click',()=>{
+         const btn = document.querySelector('#user-button')
+         btn.classList = 'btn-pointer'
+     })
+ }
+ activeBtn()
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,130 @@
 /* Desenvolva sua lógica aqui...*/
+
+
+
+function getDataLocalStorage(){
+    return JSON.parse(localStorage.getItem("@gitsearch:user")) || []
+}
+
+function getUser(){
+
+    let array = getDataLocalStorage()
+
+    let index = array.length - 1
+   
+    let user = array[index]
+    console.log(user)
+    renderPage(user)
+
+}
+getUser()
+
+function exitPage() {
+  const btn = document.querySelector('.btn-change')
+
+  btn.addEventListener('click', ()=>{
+      window.location.href = '../home/index.html'
+      body.innerHtml = ''
+  })
+}
+exitPage()
+
+
+
+
+async function renderPage(objeto){
+    let {login,avatar_url,bio,name,repos_url} = objeto
+
+    let img = document.querySelector('.user-img')
+    img.src = avatar_url
+
+    let p1 = document.querySelector('.user-name')
+    p1.innerText = name
+
+    let p2 = document.querySelector('.user-bio')
+    p2.innerText = bio
+
+    
+
+    const repos = await fetch(`${repos_url}`)
+    const reposJson = await repos.json()
+
+
+    let reposArray = []
+    reposJson.forEach(element => {
+
+        let {name,html_url,description} = element
+
+        let newElement = {
+            'name': name,
+            'html': html_url,
+            'description': description,
+        }
+
+    
+        reposArray.push(newElement)
+    });
+
+
+
+   renderRepos(reposArray)
+
+   
+}
+
+
+function renderRepos(array) {
+    const ul = document.querySelector('.repos-list')
+
+
+    array.forEach((element)=>{
+        
+        let templateRepo = createRepo(element)
+
+        ul.appendChild(templateRepo)
+    })
+}
+
+
+
+
+
+function createRepo(elemento) {
+    let {name,html,description} = elemento
+
+    let li = document.createElement("li")
+    li.classList.add('repo')
+
+    let container = document.createElement('div')
+    container.classList.add('repo-div')
+
+    let h2 = document.createElement('h2')
+    h2.classList.add('repo-title')
+    h2.innerText = name
+
+    let p = document.createElement('p')
+    p.classList.add('repo-text')
+    p.innerText = description
+
+    let divBtn = document.createElement('div')
+    divBtn.classList.add('btn-div')
+
+    let repoBtn = document.createElement('button')
+    repoBtn.classList.add('repo-btn')
+    repoBtn.innerText = 'Repositório'
+
+    repoBtn.addEventListener('click', ()=>{
+        window.location.href = html
+    })
+
+    let demoBtn = document.createElement('button')
+    demoBtn.classList.add('demo-btn')
+    demoBtn.innerText = 'Demo'
+
+
+    divBtn.append(repoBtn,demoBtn)
+    container.append(h2,p,divBtn)
+    li.appendChild(container)
+
+    return li
+}
